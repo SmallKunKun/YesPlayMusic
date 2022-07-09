89ace9636750bc1cb1397807196860debdf7a14c
SmallKunKun <54430298+SmallKunKun@users.noreply.github.com>
2022-07-09 09:06:15 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -21,7 +21,7 @@ module.exports = {
     },
   },
   pwa: {
-    name: 'YesPlayMusic',
+    name: '音乐 - SmallKunKun',
     iconPaths: {
       favicon32: 'img/icons/favicon-32x32.png',
     },
@@ -38,7 +38,7 @@ module.exports = {
       entry: 'src/main.js',
       template: 'public/index.html',
       filename: 'index.html',
-      title: 'YesPlayMusic',
+      title: '音乐 - SmallKunKun',
       chunks: ['main', 'chunk-vendors', 'chunk-common', 'index'],
     },
   },
